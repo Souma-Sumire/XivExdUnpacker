7d932c8c96c0192dad251f0f2cf62d4b34a88f94
Souma-Sumire <553469159@qq.com>
2026-02-05 18:17:26 +0800

    chore

--- src/Services/SchemaUpdater.cs
+++ src/Services/SchemaUpdater.cs
@@ -4,7 +4,7 @@ using System.IO.Compression;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace XivExdUnpacker.Services;
+namespace XivExdUnpacker.src.Services;
 
 public static class SchemaUpdater
 {
